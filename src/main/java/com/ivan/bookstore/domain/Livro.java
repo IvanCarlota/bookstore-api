2c620848a43839f47e2e22a9ddc3a58fe44f63b0
IvanCarlota <ivan.junior.carlota@gmail.com>
2023-10-09 20:35:39 -0300

    build: Repositories CategoriaRepository and LivroRepository

--- src/main/java/com/ivan/bookstore/domain/Livro.java
+++ src/main/java/com/ivan/bookstore/domain/Livro.java
@@ -14,6 +14,7 @@ import javax.persistence.ManyToOne;
 public class Livro implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;

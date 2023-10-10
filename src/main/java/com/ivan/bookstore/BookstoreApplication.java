575369fc1c86d1d8065eca823059131bb0ef6eb0
IvanCarlota <ivan.junior.carlota@gmail.com>
2023-10-10 15:30:57 -0300

    Configurado o H2, criado mock para teste com dados ficticios.

--- src/main/java/com/ivan/bookstore/BookstoreApplication.java
+++ src/main/java/com/ivan/bookstore/BookstoreApplication.java
@@ -1,13 +1,40 @@
 package com.ivan.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.ivan.bookstore.domain.Categoria;
+import com.ivan.bookstore.domain.Livro;
+import com.ivan.bookstore.repositories.CategoriaRepository;
+import com.ivan.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	public void run(String... args)throws Exception{
+		Categoria cat1 = new Categoria(null, "Teologia", "Livros de Teologia");
+		
+		Livro l1 = new Livro(null, "Entendes o que lês?", "Gordon D. Fee & Douglas Stuart", "Lorem ipsum", cat1);
+				
+		cat1.getLivros().addAll(Arrays.asList(l1));
+				
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
+	
 }
